b271f213988233b7ac0cb40d3f2816d1ef7205fe
Merna Hesham <mernahesham21010@gmail.com>
2024-03-11 12:25:28 +0200

    Day3_v1

--- src/test/java/iti_intake44/pk1/TestcaseDay3.java
+++ src/test/java/iti_intake44/pk1/TestcaseDay3.java
@@ -2,8 +2,12 @@ package iti_intake44.pk1;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import java.time.Duration;
+import java.util.List;
 
 
 /**
@@ -32,10 +36,50 @@ public class TestcaseDay3 {
     By userNameLocator = By.id("user-name");
     By userPasswordLocator = By.id("password");
     By loginBtnLocator = By.id("login-button");
+
     //Home Page Locators
+    By inventoryItemLocator = By.className("inventory_item");
     By addToCartIcon = By.className("shopping_cart_link");
 
     //Session 3
+    //Note: Task 1 Test Case
+    //Get The number of inventory items
+    @Test
+    public void testDemo() {
+        //Max browser window
+        driver.manage().window().maximize();
+
+        //Go to google
+        driver.get(URL);
+
+        //Go to website
+        driver.navigate().to(websiteURL);
+
+        //Note: username
+        driver.findElement(userNameLocator).clear();
+        driver.findElement(userNameLocator).sendKeys(userName);
+
+        //Note: user password
+        driver.findElement(userPasswordLocator).clear();
+        driver.findElement(userPasswordLocator).sendKeys(userPassword);
+
+        driver.findElement(loginBtnLocator).sendKeys(Keys.ENTER);
+
+
+        //Note:
+        List<WebElement> inventoryItems = driver.findElements(inventoryItemLocator);
+
+        // Get # product items
+        int inventoryItemCounter = inventoryItems.size();
+
+        System.out.println("Inventory Items Number: " + inventoryItemCounter);
+
+        Assert.assertEquals(inventoryItems.size(), 6, "Inventory Items Number not Equal to 6");
+
+        driver.quit();
+    }
+
+
     //Note: Task 2
     public static WebElement getAddToCartButton(WebDriver driver, String itemName) {
         //Xpath => //div[contains(@class, 'inventory_item') and .//div[contains(text(), '%s')]]
