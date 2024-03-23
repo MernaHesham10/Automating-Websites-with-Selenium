e42277fb2267bfc74b798142af18cb0bc862b642
Merna Hesham <mernahesham21010@gmail.com>
2024-03-23 17:29:33 +0200

    Day4_v2

--- src/main/java/org/example/HomePagePOM.java
+++ src/main/java/org/example/HomePagePOM.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -39,8 +40,11 @@ public class HomePagePOM {
         WebElement getPaidWE = driver.findElement(getPaidLocator);
         getPaidWE.sendKeys(Keys.ENTER);
 
-        new WebDriverWait(driver, Duration.ofSeconds(30));
+        //new WebDriverWait(driver, Duration.ofSeconds(30));
         //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'right panel-heading--price')]")));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@class,\"mob-dropdown-btn\")]")));
 
         return new DocumentPagePOM(driver);
     }
